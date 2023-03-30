84dd5e50b882626715152d88c9ddbb36ce9fe2b7
LiudmilaDankevich <Dankevich_79@mail.ru>
2023-03-30 19:36:49 +0300

    Learn_to_trade

--- pages/Learn/learn_to_trade_locators.py
+++ pages/Learn/learn_to_trade_locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 class LearnToTradePageLocator:
     LOCATOR_LEARN_TO_TRADE_TEXT = (By.CSS_SELECTOR, "div.wrap>nav>span")
+    LOCATOR_BG_LEARN_TO_TRADE_TEXT = (By.CSS_SELECTOR, "div.wrap>nav>span")
     LOCATOR_LEARN_TO_TRADE_LOGIN_BUTTON = (By.ID,
                                            "wg_loginBtn")
     LOCATOR_LEARN_TO_TRADE_TRADE_NOW_BUTTON = (By.CSS_SELECTOR,
