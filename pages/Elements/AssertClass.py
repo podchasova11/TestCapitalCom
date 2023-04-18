5a55db7d44053f72cafc98dc281cf15c47276f34
LiudmilaDankevich <Dankevich_79@mail.ru>
2023-04-18 14:11:46 +0300

    conflict_change

--- pages/Elements/AssertClass.py
+++ pages/Elements/AssertClass.py
@@ -16,20 +16,15 @@ class AssertClass(BasePage):
     page_glossary = None
     platform_url = ""
 
-<<<<<<< HEAD
+
     def assert_signup(self, d, cur_language, cur_role, cur_link):
-=======
-    def assert_signup(self, d, cur_language, cur_role, cur_item_link):
->>>>>>> master
+
 
         print(f"\n{datetime.now()}   3. Assert")
         match cur_role:
             case "NoReg" | "Reg/NoAuth":
-<<<<<<< HEAD
                 self.page_signup_login = SignupLogin(d, cur_link)
-=======
-                self.page_signup_login = SignupLogin(d, cur_item_link)
->>>>>>> master
+
                 if self.page_signup_login.should_be_signup_form(cur_language):
                     self.page_signup_login.close_signup_form()
                 elif self.page_signup_login.should_be_signup_page(cur_language):
@@ -44,17 +39,10 @@ class AssertClass(BasePage):
                 self.page_glossary.should_be_trading_platform_page(d, self.platform_url)
                 del self.page_glossary
 
-<<<<<<< HEAD
     def assert_login(self, d, cur_link):
         """Method Assert"""
         print(f"\n{datetime.now()}   3. Assert")
         self.page_signup_login = SignupLogin(d, cur_link)
-=======
-    def assert_login(self, d, cur_item_link):
-        """Method Assert"""
-        print(f"\n{datetime.now()}   3. Assert")
-        self.page_signup_login = SignupLogin(d, cur_item_link)
->>>>>>> master
         if self.page_signup_login.should_be_login_form():
             self.page_signup_login.close_login_form()
             del self.page_signup_login
