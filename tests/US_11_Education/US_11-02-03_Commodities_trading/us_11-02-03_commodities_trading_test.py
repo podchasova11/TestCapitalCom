49310c4cd305fcd7f3cefb63732ac10a59a56c8c
AnIHULI <anviruslezgin@gmail.com>
2023-04-29 00:23:16 +0300

    Some fixes. TC #6 in developing mode now

--- tests/US_11_Education/US_11-02-03_Commodities_trading/us_11-02-03_commodities_trading_test.py
+++ tests/US_11_Education/US_11-02-03_Commodities_trading/us_11-02-03_commodities_trading_test.py
@@ -325,32 +325,32 @@ class TestCommoditiesTrading:
         test_element = AssertClass(d, cur_item_link)
         test_element.assert_signup(d, cur_language, cur_role, cur_item_link)
 
-    @allure.step("Start test of button [Start trading] in article")
-    # @profile(precision=3)
-    def test_06_start_trading_in_article_button(
-            self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link,
-            prob_run_tc, cur_time):
-        """
-        Check: Button [Start trading] in article
-        Language: All. License: All.
-        """
-        print(f"\n{datetime.now()}   Работает obj {self} с именем TC_11.02.03_04")
-        build_dynamic_arg(self, d, worker_id, cur_language, cur_country, cur_role, cur_login, cur_password,
-                          prob_run_tc,
-                          "11.02.03", "Educations > Menu item [Commodities trading]",
-                          "06", "Testing button [Start trading] in article")
-
-        # page_menu = MenuSection(d, link)
-        # page_menu.menu_education_move_focus(d, cur_language)
-        # link = page_menu.sub_menu_commodities_trading_move_focus_click(d, cur_language)
-
-        test_element = ArticleStartTrading(d, cur_item_link)
-        test_element.arrange_(d, cur_item_link)
-
-        test_element.element_click()
-
-        test_element = AssertClass(d, cur_item_link)
-        test_element.assert_signup(d, cur_language, cur_role, cur_item_link)
+    # @allure.step("Start test of button [Start trading] in article")
+    # # @profile(precision=3)
+    # def test_06_start_trading_in_article_button(
+    #         self, worker_id, d, cur_language, cur_country, cur_role, cur_login, cur_password, cur_item_link,
+    #         prob_run_tc, cur_time):
+    #     """
+    #     Check: Button [Start trading] in article
+    #     Language: All. License: All.
+    #     """
+    #     print(f"\n{datetime.now()}   Работает obj {self} с именем TC_11.02.03_04")
+    #     build_dynamic_arg(self, d, worker_id, cur_language, cur_country, cur_role, cur_login, cur_password,
+    #                       prob_run_tc,
+    #                       "11.02.03", "Educations > Menu item [Commodities trading]",
+    #                       "06", "Testing button [Start trading] in article")
+    #
+    #     # page_menu = MenuSection(d, link)
+    #     # page_menu.menu_education_move_focus(d, cur_language)
+    #     # link = page_menu.sub_menu_commodities_trading_move_focus_click(d, cur_language)
+    #
+    #     test_element = ArticleStartTrading(d, cur_item_link)
+    #     test_element.arrange_(d, cur_item_link)
+    #
+    #     test_element.element_click()
+    #
+    #     test_element = AssertClass(d, cur_item_link)
+    #     test_element.assert_signup(d, cur_language, cur_role, cur_item_link)
 
     @allure.step("Start test of button [Create your account] in block [Steps trading]")
     # @profile(precision=3)
