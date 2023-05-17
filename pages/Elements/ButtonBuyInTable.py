9107ceab00abb723ab64b5ce62d047477ef4725d
AnIHULI <anviruslezgin@gmail.com>
2023-05-17 22:59:27 +0300

    .

--- pages/Elements/ButtonBuyInTable.py
+++ pages/Elements/ButtonBuyInTable.py
@@ -11,6 +11,7 @@ from pages.base_page import BasePage
 from pages.Elements.testing_elements_locators import ButtonsOnPageLocators
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
 from pages.Elements.AssertClass import AssertClass
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class BuyButtonTableMostTraded(BasePage):
@@ -74,10 +75,11 @@ class BuyButtonTableMostTraded(BasePage):
             #     'return arguments[0].scrollIntoView({block: "center", inline: "nearest"});',
             #     button_list[i]
             # )
-
+            hover = ActionChains(self.browser).move_to_element(self.browser.find_element(*self.current_tab))
             try:
                 if self.browser.find_elements(*self.current_tab):
                     self.browser.find_elements(*self.current_tab)[j].click()
+                    hover.perform()
                     if i == 4 or i == 9 or i == 14 or i == 19:
                         j += 1
                 if self.browser.find_element(*self.locator):
