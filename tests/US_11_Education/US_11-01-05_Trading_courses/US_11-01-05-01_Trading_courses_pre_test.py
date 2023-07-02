c78c8b48092c8332496454fd5a36077cfd198d4a
Alexander Tomelo <qa.tomelo.an@gmail.com>
2023-07-02 22:21:17 +0300

    corrected US_11-01-05 files

--- tests/US_11_Education/US_11-01-05_Trading_courses/US_11-01-05-01_Trading_courses_pre_test.py
+++ tests/US_11_Education/US_11-01-05_Trading_courses/US_11-01-05-01_Trading_courses_pre_test.py
@@ -3,11 +3,11 @@
 @Time    : 2023/05/25 22:00 GMT+3
 @Author  : Liudmila Dankevich
 """
-
-
 import pytest
 import allure
+import random  # for new method
 from datetime import datetime
+from pages.base_page import calc_const_and_k  # for new method
 from pages.Menu.menu import MenuSection
 from pages.Elements.testing_elements_locators import CoursesPage
 from tests.build_dynamic_arg import build_dynamic_arg_v2
@@ -30,8 +30,8 @@ class TestCoursesItemsPreset:
 
         link = build_dynamic_arg_v2(self, d, worker_id, cur_language, cur_country,
                                     cur_role, prob_run_tc,
-                                   "11.01.05.01", "",
-                                   "00", "Pretest")
+                                    "11.01.05.01", "",
+                                    "00", "Pretest")
         page_conditions = Conditions(d, "")
         page_conditions.preconditions(
             d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
@@ -43,19 +43,28 @@ class TestCoursesItemsPreset:
         page_menu = MenuSection(d, link)
         page_menu.menu_education_move_focus(d, cur_language)
         page_menu.sub_menu_trading_courses_move_focus_click(d, cur_language)
+        del page_menu
 
         # Записываем ссылки в файл
         name_file = "tests/US_11_Education/US_11-01-05_Trading_courses/list_of_href.txt"
         list_items = d.find_elements(*CoursesPage.COURSES_PAGES_LIST)
-        print(f"Trading courses include {len(list_items)} courses item(s) on page")
-        f = open(name_file, "w")
-        try:
-            for i in range(len(list_items)):
-                item = list_items[i]
-                f.write(item.get_property("href") + "\n")
-        finally:
-            f.close()
+        count_all = len(list_items)  # for new method
+        print(f"Trading courses include {count_all} courses item(s) on page")
+
+        if count_all > 0:  # for fix bug
+            const, k = calc_const_and_k(count_all)  # for new method
+
+            f = open(name_file, "w")
+            try:
+                j = 0  # for new method
+                for i in range(len(list_items)):
+                    item = list_items[i]
+                    if random.randint(1, k) <= const:  # for new method
+                        f.write(item.get_property("href") + "\n")
+                        j += 1  # for new method
+            finally:
+                f.close()
+            print(f"{datetime.now()}   Test data include {j} financial item(s)")  # for new method
+            print(f"{datetime.now()}   The probability of test coverage = {j/count_all*100} %")  # for new method
 
         count -= 1
-
-        del page_menu
