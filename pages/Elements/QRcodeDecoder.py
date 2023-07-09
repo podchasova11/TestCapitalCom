3f6624a58c65b70552515f764b3cea3275c4b6c4
AnIHULI <anviruslezgin@gmail.com>
2023-07-09 17:55:20 +0300

    flake fixes

--- pages/Elements/QRcodeDecoder.py
+++ pages/Elements/QRcodeDecoder.py
@@ -79,7 +79,6 @@ class QRCodeDecode(BasePage):
             print(f"{datetime.now()}   => Opening link from QR_CODE")
             self.browser.get(link)
 
-
             ############ Не удалять ############
             # qr_code = self.browser.find_element(*self.locator)
             # src = qr_code.get_attribute('src')
