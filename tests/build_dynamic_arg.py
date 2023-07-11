ba894154f13d1395277343708aecd73de08c07d8
Alexander Tomelo <qa.tomelo.an@gmail.com>
2023-07-11 10:11:14 +0300

    modyfi build_dynamic_arg_v2 function

--- tests/build_dynamic_arg.py
+++ tests/build_dynamic_arg.py
@@ -5,64 +5,10 @@
 """
 import pytest
 import allure
-import sys
+# import sys
 from datetime import datetime
-from pages.conditions import Conditions
-# from pages.Signup_login.signup_login import SignupLogin
-# from pages.base_page import BasePage
-from src.src import CapitalComPageSrc
 
 count = 1
-
-
-def build_dynamic_arg(obj, d, worker_id, cur_language, cur_country, cur_role, cur_login, cur_password, prob_run_tc,
-                      us, desc_feature, num_tc, desc_story):
-    """
-    function for dynamic bild names pf epic, feature and story
-    """
-    global count
-
-    tc = f"TC_{us}_{num_tc}"
-    print(d.get_window_size())
-    print(f"\n{datetime.now()}   browser = {d.name}")
-    print(f"\n{datetime.now()}   worker_id = {worker_id}")
-    print(f"\n{datetime.now()}   Start {tc}")
-    print(f"\n{datetime.now()}   {obj}.{obj.page_conditions}")
-    print(f"\n{datetime.now()}   0. Arrange")
-
-    language = cur_language
-    if cur_language == "":
-        language = "en"
-    dynamic_epic = f"(Attention! Deprecated \"build_dynamic_arg\" function. Use Ver. 2) Language: {language} / US_{us}"
-    dynamic_feature = f"Country: {cur_country} / TS_{us} | {desc_feature}"
-    dynamic_story = f"Role: {cur_role} / TC_{us}_{num_tc} | {desc_story}"
-
-    allure.dynamic.epic(dynamic_epic)
-    allure.dynamic.feature(dynamic_feature)
-    allure.dynamic.story(dynamic_story)
-    allure.dynamic.title(f"TC_{us}_{num_tc} with parameters: {language}, {cur_country}, {cur_role}")
-    del dynamic_story
-    del dynamic_feature
-    del dynamic_epic
-
-    if num_tc == "00" and count == 0:
-        return ""
-
-    obj.page_conditions = Conditions(d, "")
-    print(f"\n{datetime.now()}   {obj}.{obj.page_conditions}")
-    link = obj.page_conditions.preconditions(
-        d, CapitalComPageSrc.URL, "", cur_language, cur_country, cur_role, cur_login, cur_password)
-
-    print(f"\n{datetime.now()}   {obj}.{obj.page_conditions}")
-    print(f"{datetime.now()}   sys.getrefcount(page_conditions) = {sys.getrefcount(obj.page_conditions)}")
-    del obj.page_conditions
-    del tc
-
-    if prob_run_tc != "":
-        pytest.skip(f"{prob_run_tc}")
-
-    count -= 1
-    return link
 
 
 def build_dynamic_arg_v2(obj, d, worker_id, cur_language, cur_country, cur_role, prob_run_tc,
@@ -84,8 +30,8 @@ def build_dynamic_arg_v2(obj, d, worker_id, cur_language, cur_country, cur_role,
     if cur_language == "":
         language = "en"
     dynamic_epic = f"Language: {language} / US_{us} | {desc_feature}"
-    dynamic_feature = f"Country: {cur_country} / TS_{us} | {desc_feature}"
-    dynamic_story = f"Role: {cur_role} / TC_{us}_{num_tc} | {desc_story}"
+    dynamic_feature = f"Country: {cur_country} / Role: {cur_role} / TS_{us} | {desc_feature}"
+    dynamic_story = f"TC_{us}_{num_tc} | {desc_story}"
 
     allure.dynamic.epic(dynamic_epic)
     allure.dynamic.feature(dynamic_feature)
