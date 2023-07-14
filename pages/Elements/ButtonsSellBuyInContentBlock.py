2f370d7b063ecc91f20dae133c72a26b7cc637f2
AnIHULI <anviruslezgin@gmail.com>
2023-07-14 13:26:58 +0300

    .

--- pages/Elements/ButtonsSellBuyInContentBlock.py
+++ pages/Elements/ButtonsSellBuyInContentBlock.py
@@ -37,7 +37,7 @@ class ButtonsSellBuyInContentBlock(BasePage):
             print(f"{datetime.now()}   => BUTTON_{self.button.upper()}_IN_CONTENT_BLOCK is not visible on the page!")
             pytest.skip("Checking element is not on this page")
 
-    @allure.step(f"Click button [Buy] in content block")
+    @allure.step(f"Click button [Buy]/[Sell] in content block")
     def element_click(self, cur_role):
         button_list = self.browser.find_elements(*self.button_locator)
         # Вытаскиваем линку из кнопки
