69325c1890c2083ebe881864fc889e991fc3a705
Aleksandr Tomelo <qa.tomelo.an@gmail.com>
2023-09-25 09:29:52 +0300

    set de/de/all param-s

--- conftest.py
+++ conftest.py
@@ -45,11 +45,11 @@ def cur_role(request):
     params=[
         # "",  # "en"
         # "ar",
-        # "de",
+        "de",
         # "el",
         # "es",
         # "fr",
-        "it",
+        # "it",
         # "hu", # Magyar
         # "nl",
         # "pl",
@@ -91,11 +91,11 @@ def cur_language(request):
         # "au",  # Australia - "ASIC" - https://capital.com/?country=au
         # "gb",  # United Kingdom - "FCA" - https://capital.com/?country=gb
         # "ae",  # United Arab Emirates - "SCB" - https://capital.com/?country=ae
-        # "de",  # Germany - "CYSEC" - https://capital.com/?country=de
+        "de",  # Germany - "CYSEC" - https://capital.com/?country=de
         # "gr",  # Greece - "CYSEC" - https://capital.com/?country=gr
         # "es",  # Spain - "CYSEC" - https://capital.com/?country=es
         # "fr",  # France - "CYSEC" - https://capital.com/?country=fr
-        "it",  # Italy - "CYSEC" - https://capital.com/?country=it
+        # "it",  # Italy - "CYSEC" - https://capital.com/?country=it
         # "hu",  # Hungary - "CYSEC" - https://capital.com/?country=hu
         # "nl",  # Netherlands - "CYSEC" - https://capital.com/?country=nl
         # "pl",  # Poland - "CYSEC" - https://capital.com/?country=pl
