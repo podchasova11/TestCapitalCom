0e1a4c57c500f3a9ae8ac5668b0f23383c2f0b30
podchasova11 <Pod4asova20@gmail.com>
2023-10-20 12:31:38 +0300

    retests changed the name_file in US_11-03-01_test

--- tests/US_11_Education/US_11-03-01_trading-strategies-guide/US_11-03-01_trading-strategies-guide_test.py
+++ tests/US_11_Education/US_11-03-01_trading-strategies-guide/US_11-03-01_trading-strategies-guide_test.py
@@ -30,7 +30,7 @@ def pytest_generate_tests(metafunc):
     Fixture generation test data
     """
     if "cur_item_link" in metafunc.fixturenames:
-        name_file = "tests/US_11_Education/US_11-03-08_Trading_strategies_guide/list_of_href.txt"
+        name_file = "tests/US_11_Education/US_11-03-01_trading_strategies_guide/list_of_href.txt"
 
         list_item_link = list()
         try:
